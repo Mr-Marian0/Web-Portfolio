02cbce036bf9b5b0a17fe6af9bd9e04b2ffaf904
Ram-MUS <ponchopnanbimbo@gmail.com>
2025-08-24 13:35:31 +0800

    rendered the projects to PROJECT CONTAINER

--- main.js
+++ main.js
@@ -6,8 +6,13 @@ const tools_used = document.querySelector('.tools_used');
 const project_lists = document.querySelector('.project_lists');
 
 const Main_Projects = [
+
+    {projectClass: "project1", jobType: "Mapping, Field Work and Web Development", image: "image/projects/mamuric2.png",
+    description: "Created a map for Mamuric7 internet provider highlighting the locations of Network Access Points from different locations. We also developed a local server website using XAMPP that allows the users/admins to perform CRUD operations.",
+    toolsUsed: ["html","css", "javascript", "php", "xampp"] },
+
     {projectClass: "project2", jobType: "Thesis / Capstone", image: "image/projects/thesis1.png",
-    description: "Developed a local server website for laboratory assistant",
+    description: "We developed a local server website for laboratory assistant",
     toolsUsed: ["html","css", "javascript", "nodeJs", "git", "github", "MYSQL"] },
 ]
 
@@ -25,12 +30,12 @@ function renderProjects(){
         const HTMLcontent = `<div class=${element.projectClass}>
                     <img src=${element.image}>
                     <div class="project_des">
-                            <h3>Type of Job: ${element.jobType}</h3>
+                            <h3><span>Type of Job: </span>${element.jobType}</h3>
                             <p><span>Job Description:</span> ${element.description}</p>
                             <div class="project_tools">
-                                <p>Tools:</p>
+                                <p><span>Tools:</span></p>
                                 <div class="tools_used">
-                                    
+                                    ${element.toolsUsed.map(Tool => tools[`${Tool}`]).join("")}
                                 </div>
                             </div>
                     </div>
@@ -38,12 +43,4 @@ function renderProjects(){
 
         project_lists.insertAdjacentHTML("beforeend", HTMLcontent);
     });
-}
-
-
-
-toolsUsed([]);
-
-function toolsUsed(a){
-    return console.log(a)
 }
